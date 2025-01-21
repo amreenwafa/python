deb5f504ebbcc608ddb3925bdcce1b77dc453458
Amreen wafa <wafaamreen6@gmail.com>
2025-01-21 22:38:10 +0530

    completed 1St day task

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 1/task/task.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 1/task/task.py
@@ -1,2 +1,7 @@
-# Write your code below this line 👇
-print("Hello World")
+print("Welcome to the band name")
+
+city = input("In which city do you live? \n")
+
+pet = input("Which pet do you have? \n")
+
+print("Your band name is " +city+ " " +pet)
